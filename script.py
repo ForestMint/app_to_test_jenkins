71a4dd62d8c840f07569bac0fd14bf8483efdaa2
ForestMint <90900993+ForestMint@users.noreply.github.com>
2025-07-04 22:24:48 +0200

    function convert_decimal_to_digital written

--- script.py
+++ script.py
@@ -1,2 +1,15 @@
 print("Hello World!")
 print("Hi")
+
+
+def convert_decimal_to_digital(decimal):
+  value = 128
+  digital = ""
+  while decimal !=0 :
+    if decimal >= value :
+      digital += "1"
+    else :
+      digital +="0"
+    value /=2
+
+  return digital
